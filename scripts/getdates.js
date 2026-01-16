f61b7039fcf3df497c9e413c91b9d8a2ab87cec2
hgregori <hgregorio@byupathway.edu>
2026-01-16 19:53:34 -0300

    js done?

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,8 @@
 
-// Create a Date object for today
 const today = new Date();
 
-// Display the current year in the footer
 document.querySelector('#currentYear').textContent = today.getFullYear();
 
-// Display the last modified date of the document
 document.querySelector('#lastModified').textContent = `Last Modified: ${document.lastModified}`;
 
 
