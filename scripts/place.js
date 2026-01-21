97d1c0a79a74873741fde07f390a57a213e560b4
hgregori <hgregorio@byupathway.edu>
2026-01-21 17:11:04 -0300

    first_point

---
+++ scripts/place.js
@@ -0,0 +1,5 @@
+const today = new Date();
+
+document.querySelector('#currentYear').textContent = today.getFullYear();
+
+document.querySelector('#lastModified').textContent = `Last Modified: ${document.lastModified}`;
