ff7c620cdef67127c55cf745d93ee3e9e8e1373b
hgregori <hgregorio@byupathway.edu>
2026-01-22 00:20:35 -0300

    js changes

--- scripts/place.js
+++ scripts/place.js
@@ -3,3 +3,19 @@ const today = new Date();
 document.querySelector('#currentYear').textContent = today.getFullYear();
 
 document.querySelector('#lastModified').textContent = `Last Modified: ${document.lastModified}`;
+
+let windChillValue = 0
+let windValue = 6
+let conditionsValue = "Dry"
+let temperatureValue = 20
+
+if (temperatureValue <= 10 && windValue > 4.8){
+    windChillValue = 13.12 + 0.6215 * temperatureValue - 11.37 * windValue**0.16 + 0.3965 * temperatureValue * windValue**0.16
+} else {
+    windChillValue = "N/A"
+}
+
+document.querySelector("#windChill").textContent = `${windChillValue} °C`
+document.querySelector("#wind").textContent = `${windValue} Km/h`
+document.querySelector("#temperature").textContent = `${temperatureValue} °C`
+document.querySelector("#conditions").textContent = conditionsValue
